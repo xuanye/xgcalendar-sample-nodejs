1808dc3a4a409188d4bc22108000a595a9d293ae
'Xuanye Wang' <'xuanye.wan@gmail.com'>
2014-07-18 09:51:17 +0800

    修复点小问题

--- static/js/page/index.js
+++ static/js/page/index.js
@@ -2,11 +2,11 @@ define(function(require, exports, module) { //参数名字不能改
   var minicalendar = require("../plugin/minicalendar");
   //require("plugin/xgcalendar");
   require("dailog");
-  exports.init =function() {   
+  exports.init =function() {
      var minical =new minicalendar({
         onchange:datechange
      });
-     minical.init("#minical");    
+     minical.init("#minical");
      var op = {
         view: "week",
         theme:1,
@@ -14,23 +14,23 @@ define(function(require, exports, module) { //参数名字不能改
         showday: new Date(),
         EditCmdhandler:edit,
         //DeleteCmdhandler:dcal,
-        ViewCmdhandler:view,    
+        ViewCmdhandler:view,
         onWeekOrMonthToDay:wtd,
         onBeforeRequestData: cal_beforerequest,
         onAfterRequestData: cal_afterrequest,
-        onRequestDataError: cal_onerror, 
-        url: "",//"/calendar/query" ,  
-        quickAddUrl: "/calendar/add" ,  
-        quickUpdateUrl: "/calendar/update" ,  
+        onRequestDataError: cal_onerror,
+        url: "/calendar/query" ,  
+        quickAddUrl: "/calendar/add" ,
+        quickUpdateUrl: "/calendar/update" ,
         quickDeleteUrl:  "/calendar/delete" //快速删除日程的
         /* timeFormat:" hh:mm t", //t表示上午下午标识,h 表示12小时制的小时，H表示24小时制的小时,m表示分钟
-        tgtimeFormat:"ht" //同上 */ 
+        tgtimeFormat:"ht" //同上 */
     };
     var _MH = document.documentElement.clientHeight;
     op.height = _MH-90;
     op.eventItems =[];
     var p = $("#xgcalendarp").bcalendar(op).BcalGetOp();
-    if (p && p.datestrshow) {       
+    if (p && p.datestrshow) {
         $("#dateshow").text(p.datestrshow);
     }
     $("#addcalbtn").click(function(){
@@ -38,14 +38,14 @@ define(function(require, exports, module) { //参数名字不能改
             $("#xgcalendarp").BCalReload();
         }});
     });
-    $("#daybtn").click(function() {      
-       switchview.call(this,"day");       
+    $("#daybtn").click(function() {
+       switchview.call(this,"day");
     });
-    $("#weekbtn").click(function() {      
-       switchview.call(this,"week");       
+    $("#weekbtn").click(function() {
+       switchview.call(this,"week");
     });
-    $("#monthbtn").click(function() {       
-       switchview.call(this,"month");       
+    $("#monthbtn").click(function() {
+       switchview.call(this,"month");
     });
     $("#prevbtn").click(function(){
         var p = $("#xgcalendarp").BCalPrev().BcalGetOp();
@@ -83,24 +83,24 @@ define(function(require, exports, module) { //参数名字不能改
         }
     }
     function cal_beforerequest(type)
-    {          
+    {
         var t=loadingmsg;
         switch(type)
         {
             case 1:
                 t=loadingmsg;
                 break;
-            case 2:                      
-            case 3:  
-            case 4:    
-                t=processdatamsg;                                   
+            case 2:
+            case 3:
+            case 4:
+                t=processdatamsg;
                 break;
         }
         $("#errorpannel").hide();
-        $("#loadingpannel").html(t).show();        
+        $("#loadingpannel").html(t).show();
     }
     function cal_afterrequest(type)
-    {         
+    {
         switch(type)
         {
             case 1:
@@ -112,27 +112,27 @@ define(function(require, exports, module) { //参数名字不能改
                 $("#loadingpannel").html(sucessmsg);
                 window.setTimeout(function(){ $("#loadingpannel").hide();},2000);
             break;
-        }              
+        }
     }
     function cal_onerror(type,data)
-    {   
-      $("#errorpannel").show(); 
+    {
+      $("#errorpannel").show();
     }
     function edit(data)
-    { 
+    {
         if(data)
         {
-            var url = StrFormat("/calendar/edit/{0}?start={2}&end={3}&isallday={4}&title={1}",data);           
+            var url = StrFormat("/calendar/edit/{0}?start={2}&end={3}&isallday={4}&title={1}",data);
             OpenModalDialog(url, { caption: "修改活动", width: 580, height: 460, onclose: function(){
                 $("#xgcalendarp").BCalReload();
-            }});           
+            }});
         }
-    }    
+    }
     function view(data)
-    {              
-    }    
+    {
+    }
     function dcal(data,callback)
-    {            
+    {
     }
     function wtd(p)
     {
@@ -142,7 +142,7 @@ define(function(require, exports, module) { //参数名字不能改
        $("#viewswithbtn button.current").each(function() {
             $(this).removeClass("current");
         })
-        $("#daybtn").addClass("current");             
+        $("#daybtn").addClass("current");
     }
   }
 });
